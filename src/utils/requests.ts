2fef89c082f6cabd0232cf0163eddf332ef452af
Misbah Zarrar <61513813+zarrar1607@users.noreply.github.com>
2024-09-01 20:18:08 +0000

    Bug 7 partial fix

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -5,56 +5,63 @@ import {
   SetTransactionApprovalParams,
   Transaction,
   Employee,
-} from "./types"
-import mockData from "../mock-data.json"
+} from "./types";
+import mockData from "../mock-data.json";
 
-const TRANSACTIONS_PER_PAGE = 5
+const TRANSACTIONS_PER_PAGE = 5;
 
 const data: { employees: Employee[]; transactions: Transaction[] } = {
   employees: mockData.employees,
   transactions: mockData.transactions,
-}
+};
 
-export const getEmployees = (): Employee[] => data.employees
+export const getEmployees = (): Employee[] => data.employees;
 
 export const getTransactionsPaginated = ({
   page,
 }: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
   if (page === null) {
-    throw new Error("Page cannot be null")
+    throw new Error("Page cannot be null");
   }
 
-  const start = page * TRANSACTIONS_PER_PAGE
-  const end = start + TRANSACTIONS_PER_PAGE
+  const start = page * TRANSACTIONS_PER_PAGE;
+  const end = start + TRANSACTIONS_PER_PAGE;
 
   if (start > data.transactions.length) {
-    throw new Error(`Invalid page ${page}`)
+    throw new Error(`Invalid page ${page}`);
   }
 
-  const nextPage = end < data.transactions.length ? page + 1 : null
+  const nextPage = end < data.transactions.length ? page + 1 : null;
 
   return {
     nextPage,
-    data: data.transactions.slice(start, end),
-  }
-}
+    data: data.transactions.slice(0, end),
+  };
+};
 
-export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+export const getTransactionsByEmployee = ({
+  employeeId,
+}: RequestByEmployeeParams) => {
   if (!employeeId) {
-    throw new Error("Employee id cannot be empty")
+    throw new Error("Employee id cannot be empty");
   }
 
-  return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
-}
+  return data.transactions.filter(
+    (transaction) => transaction.employee.id === employeeId
+  );
+};
 
-export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams): void => {
+export const setTransactionApproval = ({
+  transactionId,
+  value,
+}: SetTransactionApprovalParams): void => {
   const transaction = data.transactions.find(
     (currentTransaction) => currentTransaction.id === transactionId
-  )
+  );
 
   if (!transaction) {
-    throw new Error("Invalid transaction to approve")
+    throw new Error("Invalid transaction to approve");
   }
 
-  transaction.approved = value
-}
+  transaction.approved = value;
+};
